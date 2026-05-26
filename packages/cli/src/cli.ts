c499f59e938ab01a41119fe3d44992b1e8cd0aa3
onurkerem <o.keremorenli@gmail.com>
2026-05-26 14:24:20 +0300

    scaffold: initial project structure with dependencies

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("auto-image-editor")
+  .description("CLI tool for adding text overlays to images")
+  .version("0.1.0");
+
+program.parse();
